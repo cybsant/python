ea6f32b0bbbf4d4b3df8575290014718c10fe046
Cybsant <cybsant@gmail.com>
2023-08-21 15:56:20 +0300

    4.2.1

--- synergy/Урок_4_Задание_2.py
+++ synergy/Урок_4_Задание_2.py
@@ -1,9 +1,15 @@
-## ввод
+## ввод числа
 souce = int (input ('5-значное число: '))
+# "Разложение" числа на цифры
+source_txt = str(source)
+d1 = num_str[0]
+d2 = num_str[1]
+d3 = num_str[2]
+d4 = num_str[3]
+d5 = num_str[4]
+# 
 
-
-
-
-
+# Операции
+r1 = int(source*d3//(d1-d2))
 ## final
-print (f'Результат: ' {final})
+print (f'Результат: ' {r1})

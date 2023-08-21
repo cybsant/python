fcea7b3f26ffe15b000e06a8ed2229842a82651f
Cybsant <cybsant@gmail.com>
2023-08-22 01:03:06 +0300

    4.2

--- synergy/Урок_4_Задание_2.py
+++ synergy/Урок_4_Задание_2.py
@@ -1,17 +1,17 @@
 ## ввод числа 
-source = float(input('5-значное число: '))
+src = float(input('5-значное число: '))
 # "Разложение" числа на цифры
-source_txt = str(source)
-d1 = float(source_txt[0])
-d2 = float(source_txt[1])
-d3 = float(source_txt[2])
-d4 = float(source_txt[3])
-d5 = float(source_txt[4])
+src_txt = str(src)
+d1 = float(src_txt[0])
+d2 = float(src_txt[1])
+d3 = float(src_txt[2])
+d4 = float(src_txt[3])
+d5 = float(src_txt[4])
 # print(d1, d2, d3, d4, d5)
 # Операции
 r0 = d4 ** d5
 r1 = r0 * d3
 r2 = d1 - d2
 r3 = r1 // r2
-## final
+## Вывод
 print(f'Результат:', r3)

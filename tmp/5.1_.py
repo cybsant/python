4e9144d9a1628c8d4e19ee0c850fa539ec6cf6fa
cybsant <cybsant@yandex.ru>
2023-08-29 21:18:32 +0300

    l520

---
+++ tmp/5.1_.py
@@ -0,0 +1,10 @@
+x = int(input())
+y = int(input())
+
+if x < y:
+    print("less")
+else:
+    if x == y:
+        print("equal")
+    else:
+        print("greater")
---
+++ tmp/6.1_.py
@@ -0,0 +1,6 @@
+cola = int(input())
+client = int(input())
+cnt = 0
+ 
+while (clients >= 0) and (cola >= 0):
+    cans = int(input())

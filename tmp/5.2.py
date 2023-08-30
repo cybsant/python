092a73fa41d8f45d2aec1b001ff6ae37aae35a64
cybsant <cybsant@yandex.ru>
2023-08-30 18:09:56 +0300

    5.1

--- tmp/5.2.py
+++ tmp/5.2.py
@@ -1,2 +1,16 @@
-word = str(input())
+## Дано
+word = str(input("Введите слово: ").lower())
+sg = {'a', 'e', 'i', 'o', 'u'}
 
+
+## Разложение слова на буквы
+num_src = len(word)
+print("Всего букв:", num_src)
+
+num_cnt = 0
+if 
+
+
+## Считаем
+sg_cnt = 0
+gl_cnt = 0

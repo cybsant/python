ed3f9d5ca5532da80ea1b000fd1dadd84042e15a
cybsant <cybsant@yandex.ru>
2023-09-03 20:10:03 +0000

    xz

--- tmp/5.2.py
+++ tmp/5.2.py
@@ -1,4 +1,4 @@
-# Дано
+# """ Дано """
 WORD = str(input("Введите слово: ").lower())
 GL_CNT = 0
 A_CNT = 0
@@ -9,38 +9,38 @@ U_CNT = 0
 SG_CNT = 0
 NUM_CNT = 0
 NUM_SRC = len(WORD)
-# Считаем
-while (NUM_CNT < NUM_SRC): # Пока не закончатся буквы
-    if (WORD[NUM_CNT] == 'a'):
+# """ Считаем """
+while NUM_CNT < NUM_SRC: # Пока не закончатся буквы
+    if WORD[NUM_CNT] == 'a':
         GL_CNT += 1
         A_CNT += 1
-    elif (WORD[NUM_CNT] == 'e'):
+    elif WORD[NUM_CNT] == 'e':
         GL_CNT += 1
         E_CNT += 1
-    elif (WORD[NUM_CNT] == 'i'):
+    elif WORD[NUM_CNT] == 'i':
         GL_CNT += 1
         I_CNT += 1
-    elif (WORD[NUM_CNT] == 'o'):
+    elif WORD[NUM_CNT] == 'o':
         GL_CNT += 1
         O_CNT += 1
-    elif (WORD[NUM_CNT] == 'u'):
+    elif WORD[NUM_CNT] == 'u':
         GL_CNT += 1
         U_CNT += 1
     else:
         SG_CNT += 1
     NUM_CNT += 1
-# Замена 0 на false
-if (A_CNT == 0):
+# """ Замена 0 на false """
+if A_CNT == 0:
     A_CNT = 'False'
-if (E_CNT == 0):
+if E_CNT == 0:
     E_CNT = 'False'
-if (I_CNT == 0):
+if I_CNT == 0:
     I_CNT = 'False'
-if (O_CNT == 0):
+if O_CNT == 0:
     O_CNT = 'False'
-if (U_CNT == 0):
+if U_CNT == 0:
     U_CNT = 'False'
-# Вывод
+# """ Вывод """
 print("Всего букв:", NUM_SRC)
 print("Согласных:", SG_CNT)
 print("Гласных:", GL_CNT)
--- tmp/6.1_.py
+++
@@ -1,5 +0,0 @@
-cola = int(input())
-client = int(input())
-cnt = 0
-while (client >= 0) and (cola >= 0):
-    cans = int(input())

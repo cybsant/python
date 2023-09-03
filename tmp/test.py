1779ad15c380a3f283be3c6bab4a8c9c22f01b5e
Cybsant <cybsant@gmail.com>
2023-09-03 21:39:01 +0300

    fetch

---
+++ tmp/test.py
@@ -0,0 +1,13 @@
+word = str(input("Введите слово: ").lower())
+num_src = len(word)
+
+gl = {'a', 'e', 'i', 'o', 'u'}
+
+num_cnt = 0
+sg_cnt = 0
+gl_cnt = 0
+while (num_cnt < num_src):
+    print(num_cnt, word[num_cnt])
+	if (word[num_cnt] != gl)
+    num_cnt = num_cnt+1
+print("Всего букв:", num_src)

46fd706a1c4321b9f86d2a5b09c00f3448ef4b7d
cybsant <cybsant@gmail.com>
2023-10-02 18:17:55 +0300

    Understanding

--- Tkinter/calc.py
+++ Tkinter/calc.py
@@ -11,16 +11,23 @@ buttons = (('1', '2', '3', '/', '4'),
            ('.', '0', '=', '+', '4')
            )
 
+# Для составления чисел при нажатии кнопок
 activeStr = ''
+# Стэк для хранения операций и операндов
 stack = []
 
+# Арифметика
 def calculate():
     global stack
     global label
-    result = 0
-    operand2 = Decimal(stack.pop())
-    operation = stack.pop()
+    # Извлекает первый операнд из стэка
     operand1 = Decimal(stack.pop())
+    # Извлекает последнее значение в стэке как второй операнд
+    operand2 = Decimal(stack.pop())
+    # Извлекает действие в стэке (должно быть +,-,* или /)
+    operation = stack.pop()
+
+    result = 0
 
     if operation == '+':
         result = operand1 + operand2
@@ -30,19 +37,25 @@ def calculate():
         result = operand1 / operand2
     if operation == '*':
         result = operand1 * operand2
+    # Обновляем текст на дисплее 
     label.configure(text=str(result))
 
+# Функция для обработки нажатия кнопок
 def click(text):
     global activeStr
     global stack
+    # Если текст кнопки 'CE', то очищаем activeStr и стэк, выводим '0' на дисплее 
     if text == 'CE':
         stack.clear()
         activeStr = ''
         label.configure(text='0')
+    # Нажимаем на цифровую кнопку
     elif '0' <= text <= '9':
         activeStr += text
         label.configure(text=activeStr)
+    # Нажимаем точку
     elif text == '.':
+        # Не ставим вторую точку
         if activeStr.find('.') == -1:
             activeStr += text
             label.configure(text=activeStr)
@@ -62,39 +75,35 @@ def click(text):
                 activeStr = ''
                 label.configure(text='0')
 
-# Interface
-#root.configure(bg="gray") # меняем цвет фона
+# Задаем стиль интерфейса
 ttk.Style().theme_use("clam")
+
+# Создаем объект стиля для настройки виджетов
 label_style = ttk.Style()
+# Создаем стиль для дисплея
 label_style.configure("My.TLabel",
                     font="helvetica 14",
                     foreground="#004D40",
                     padding=4,
-                    background="#B2DFDB",
-                    border = True)
+                    background="#B2DFDB")
 
-# фрейм для строки с дисплеем и кнопкой "CE"
+# Создаем фрейм для дисплея и кнопки "CE"
 frame = ttk.Frame(root)
 frame.grid(row=0, column=0, columnspan=4, sticky="nsew", padx=2, pady=2)
 
-# дисплей на левую сторону фрейма
-label = ttk.Label(frame, text='0', width=24, style="My.TLabel" )
+# Создаем дисплей
+label = ttk.Label(frame, text='0', width=20, style="My.TLabel")
 label.grid(row=0, column=0, sticky="w", padx=2, pady=2)
 
-# кнопку "CE" на правую сторону фрейма
-button = ttk.Button(frame, text='CE', command=lambda text='CE': click(text), width=8)
-button.grid(row=0, column=1, sticky="e", padx=2, pady=2)  # расстояние между кнопками
+# Создаем кнопку "CE"
+button = ttk.Button(frame, text='CE', command=lambda text='CE': click(text), width=10)
+button.grid(row=0, column=1, sticky="e", padx=2, pady=2)
 
+# Создаем кнопки калькулятора
 for row in range(4):
     for col in range(4):
         button_text = buttons[row][col]
-        if button_text == '=':
-            button = ttk.Button(root, text=button_text, command=lambda text=button_text: click(text), width=3)
-        else:
-            button = ttk.Button(root, text=button_text, command=lambda text=button_text: click(text), width=3)
-        button.grid(row=row + 1, column=col, sticky="nsew", padx=2, pady=2)  # расстояние между кнопками
-
-root.grid_rowconfigure(5, weight=1)
-root.grid_columnconfigure(4, weight=1)
+        button = ttk.Button(root, text=button_text, command=lambda text=button_text: click(text), width=3)
+        button.grid(row=row + 1, column=col, sticky="nsew", padx=2, pady=2)
 
 root.mainloop()

580da9b79d8eb663eb312f217038825bc8836b42
cybsant <cybsant@gmail.com>
2023-10-02 12:58:02 +0300

    k

--- gtcalc/gtcalc.py
+++ gtcalc/gtcalc.py
@@ -1,7 +1,7 @@
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Pango
-from gi.repository import Gdk
+#from gi.repository import Gdk
 from decimal import Decimal
 
 class Calculator(Gtk.Window):

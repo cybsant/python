53359d5e584f2f11623d935dd8cf585af4314190
cybsant <cybsant@gmail.com>
2023-10-03 16:02:07 +0300

    10.1.my

--- synergy/Урок_10_Задание_1.py
+++ synergy/Урок_10_Задание_1.py
@@ -14,18 +14,13 @@ pets = {
 }
 
 """ Добавляем к возрасту правильное слово """
-if age == 1:
-    pets[name]["Возраст"] = str(pets[name]["Возраст"]) + " год"
-elif 2 <= age <= 4:
-    pets[name]["Возраст"] = str(pets[name]["Возраст"]) + " года"
-else:
-    pets[name]["Возраст"] = str(pets[name]["Возраст"]) + " лет"
-
-""" А почему нельзя так? """
-# print (f'Это {pets[name]["Вид"]} по кличке "{name}". Возраст: {pets[name]["Возраст"]}. Владелец: {pets[name]["Владелец"]}')
-""" Этот вариант пришел в голову моментально """
+pets[name]["Возраст"] = f'{pets[name]["Возраст"]} лет' if age > 4 else f'{pets[name]["Возраст"]} год' if age == 1 else f'{pets[name]["Возраст"]} года'
 
 """ Не придумал как тут применить keys(), но вот c использованием items() и values() """
 pet_name, pet_info = list(pets.items())[0]
 pet_subj, pet_age, pet_owner = pet_info.values()
 print(f'Это {pet_subj} по кличке "{pet_name}". Возраст: {pet_age}. Владелец: {pet_owner}')
+
+""" А почему нельзя так? """
+# print (f'Это {pets[name]["Вид"]} по кличке "{name}". Возраст: {pets[name]["Возраст"]}. Владелец: {pets[name]["Владелец"]}')
+""" Этот ^^^ вариант пришел в голову моментально да и понятнее так. """

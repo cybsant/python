b7a95ed58b24eacb20a4a2b9e29eb008391e9781
cybsant <cybsant@gmail.com>
2023-10-12 23:37:15 +0300

    start 16.2

--- tmp/test.py
+++ tmp/test.py
@@ -11,19 +11,19 @@ class Tortuga:
         self.y = y - s
 
     def go_left(self, x, s): # уменьшает x на s
-        self.y = y + s
+        self.x = x + s
 
     def go_right(self, x, s): # увеличивает x на s
-        self.y = y + s
+        self.x = x + s
 
     def evolve(self, s): # увеличивает s на 1
             self.s += s
 
-    def degrade(self): # уменьшает s на 1 или выкидывает ошибку, когда s может стать ≤ 0
+    def degrade(self, s): # уменьшает s на 1 или выкидывает ошибку, когда s может стать ≤ 0
         if self.s <= 0:
             return False
         else:
             self.s -= s
 
-    def count_moves(x2, y2): # возвращает минимальное количество действий, за которое черепашка сможет добраться до x2 y2 от текущей позиции
-        
+    def count_moves(x2, y2): # количество действий, чтоб добраться до x2 y2 от текущей позиции
+        return

e2b5c0bf3627a1abc926dd590cee143d2eb98f32
cybsant <cybsant@gmail.com>
2023-10-12 23:36:35 +0300

    start 16.2

--- tmp/Class_Human.py
+++ tmp/Class_Human.py
@@ -1,26 +1,30 @@
 class Human(object):
+    # Статические атрибуты
     name = "Ivan"
     height = 175
     age = 25
 
     def __init__(self, n, h, a):
+        # Динамические атрибуты
         self.name = n
         self.height = h
         self.age = a
 
+    # Методы класса
     def privet(self):
         print(f'Hello, my name is {self.name} and I am {self.age} years old!')
-    
+
     def get_older(self):
         self.age += 5
 
     def goodbye(self):
         print('Goodbye')
 
-# Создаем обьекты класса
+# Обьекты класса
 h1 = Human('Anton', 120, 12)
 h1 = Human('Dima', 190, 23)
-# Вызываем методы
+
+# Вызов методов
 h1.privet()
 h1.get_older()
 h1.privet()
--- tmp/test.py
+++ tmp/test.py
@@ -1,11 +1,29 @@
-class Transport:
-    def __init__(self, name, max_speed, mileage):
-        self.name = name
-        self.max_speed = max_speed
-        self.mileage = mileage
+class Tortuga:
+    def __init__(self, x, y, s):
+        self.x = x
+        self.y = y
+        self.s = s
 
-# Создаем объект класса Transport
-autobus = Transport('Renault Logan', 180, 12)
+    def go_up(self, y, s): # увеличивает y на s
+        self.y = y + s
 
-# Вывод
-print(f'Название автомобиля: {autobus.name}, Скорость: {autobus.max_speed}. Пробег: {autobus.mileage}.')
+    def go_down(self, y, s): # уменьшает y на s
+        self.y = y - s
+
+    def go_left(self, x, s): # уменьшает x на s
+        self.y = y + s
+
+    def go_right(self, x, s): # увеличивает x на s
+        self.y = y + s
+
+    def evolve(self, s): # увеличивает s на 1
+            self.s += s
+
+    def degrade(self): # уменьшает s на 1 или выкидывает ошибку, когда s может стать ≤ 0
+        if self.s <= 0:
+            return False
+        else:
+            self.s -= s
+
+    def count_moves(x2, y2): # возвращает минимальное количество действий, за которое черепашка сможет добраться до x2 y2 от текущей позиции
+        

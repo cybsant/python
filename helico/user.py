1e5f071431b0812b9e70eea7ee3206a97c14474b
cybsant <cybsant@gmail.com>
2023-11-11 13:50:49 +0300

    end

--- helico/user.py
+++
@@ -1,9 +0,0 @@
-# 🚁 - вертолет 🚒🚁🛩
-from utils import randcell
-
-class Helicopter:
-    def __int__(self, w, h):
-        rc = randcell(w, h)
-        rx, ry = rc[0], rc[1]
-        self.x = rx
-        self.y = ry
